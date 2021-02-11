66ee3dfe57105446fcc405128c515674d555dd2a
baitkul <bakir.aitkul@gmail.com>
2021-02-11 18:00:37 +0600

    first commit

---
+++ ava.config.js
@@ -0,0 +1,7 @@
+export default {
+  files: ['lib/**/*.spec.js', 'lib/**/*.test.js'],
+  concurrency: 5,
+  verbose: true,
+  failFast: true,
+  extensions: ['js']
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const Config = require('./lib/config')
+
+const main = async () => {
+  process.on('unhandledRejection', err => {
+    console.error(err)
+    process.exit(1)
+  })
+
+  const server = require('fastify')(Config.fastifyInit)
+
+  server.register(require('./lib/app'), Config)
+
+  await server.listen(Config.fastify)
+
+  for (const signal of ['SIGINT', 'SIGTERM']) {
+    process.on(signal, () =>
+      server.close().then(err => {
+        console.log(`close application on ${signal}`)
+        process.exit(err ? 1 : 0)
+      })
+    )
+  }
+}
+
+main()
